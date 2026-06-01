5d8287cf6d4b73f4d002bc1ca1184005c4d25acf
Sudhanshu Raj <rajsudhanshu987@gmail.com>
2026-06-01 20:35:36 +0530

    fix hamburger

--- roger.js
+++ roger.js
@@ -32,7 +32,12 @@
         });
     }
 
-  
+    const mobileBtn = document.getElementById("mobileBtn");
+    const mobileMenu = document.getElementById("mobileMenu");
+
+    mobileBtn?.addEventListener("click", () => {
+        mobileMenu.classList.toggle("hidden");
+    });
     toggleModal(
         "loginSeekerBtn",
         "seekerModal",
