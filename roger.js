eb71d3fa5695d95b804c5dcb133503b35993f66a
Sudhanshu Raj <rajsudhanshu987@gmail.com>
2026-06-01 18:09:11 +0530

    Added logic

--- roger.js
+++ roger.js
@@ -1,1 +1,11 @@
-﻿
+﻿function toggleModal(openBtnId, modalId, closeSelectors) {
+    const openBtn = document.getElementById(openBtnId);
+    const modal = document.getElementById(modalId);
+    const closeElements = modal.querySelectorAll(closeSelectors);
+
+    if(!modal || !openBtn) return;
+
+    openBtn.addEventListener('click', () => {
+        modal.classList.remove('hidden');
+        modal.classList.add('flex');
+    });
